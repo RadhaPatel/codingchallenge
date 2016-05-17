7fad4184ce2688e3f3556c52ad13e1d4822a83d0
Radha Patel <radhapatel@Radhas-MacBook-Pro.local>
2016-05-17 15:36:00 -0500

    working on question 1

---
+++ day1.java
@@ -0,0 +1,25 @@
+public Class Day1{
+	 
+	 /*
+	 	Problem 1: From a given string of words, get the top n occuring words. 
+	 	Suppose you are given a book, get the top n occurring words from it.
+	 */ 
+
+	 // Radha 
+	public Class wordInfo(){
+		int rank; 
+		int frequency; 
+	}
+	
+	public void getTopNWords(List<String> words){
+		List<String> topWords = new List<String>(); 
+		
+	}
+
+
+
+
+	 public void main(String[] args){
+
+	 }
+}
